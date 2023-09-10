acbe89dee32f78b18516899f37a65f9db52f97de
jan godar <2017sofin@gmail.com>
2023-09-10 23:04:57 +0300

    step 5 wrong

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import random
+import prompt
+
+def main():
+    #Приветствие
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}') 
+
+
+    print('Answer "yes" if the number even, otherwise anser "no".')
+    counter = 0
+
+    while counter != 3:
+     random_number = random.randint(1, 100)
+     print(f'Question: {random_number}')
+     user_answer = input('Your answer: ')
+
+     if user_answer not in ['yes', 'no']:
+      print(f'"{user_answer}" is wrong answer')
+
+     elif random_number % 2 == 0:
+      if user_answer == 'yes':
+       print('Correct!')
+       counter += 1
+      elif user_answer == 'no':
+       print(f'"{user_answer}" is wrong answer ;(. Correct answer was "yes".')
+
+     elif random_number % 2 != 0:
+      if user_answer == 'yes':
+       print(f'"{user_answer}" is wrong answer ;(. Correct answer was "no"')
+      elif user_answer == 'no':
+       print('Correct!')
+       counter += 1
+
+    print(f'Congradulations, {name}!')
+
+if __name__ == "__main__":
+    main()
