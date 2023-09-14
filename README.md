51a8e0dd7bfbc5b1220b21db400d3dcb5ccb5565
jan godar <2017sofin@gmail.com>
2023-09-14 16:25:26 +0300

    step 6

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/JanGodar/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/JanGodar/python-project-49/actions)
+
+
+https://asciinema.org/connect/2b4db65f-f754-47ab-912c-cc37d2109537
+
+step 6 (brain-calc): https://asciinema.org/a/INehtgnZKUXR2ytWv2Rl0j3Ut
+
+
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import random
+import prompt
+
+def main():
+    #Приветствие
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+
+    print('What is the result of the expression?')
+    counter = 0
+
+    while counter != 3:
+        first_random_number = random.randint(1, 100)
+        second_random_number = random.randint(1, 100)
+        index_operator = random.randint(1, 3)
+        if index_operator == 1:
+            correct_answer = first_random_number + second_random_number
+            print(f'Question: {first_random_number} + {second_random_number}')
+        elif index_operator == 2:
+            correct_answer = first_random_number - second_random_number
+            print(f'Question: {first_random_number} - {second_random_number}')
+        else:
+            correct_answer = first_random_number * second_random_number
+            print(f'Question: {first_random_number} * {second_random_number}')
+        
+        user_answer = int(input('Your answer '))
+
+        if user_answer == correct_answer:
+            print('Correct!')
+            counter += 1
+        else:
+            print(f"{user_answer} is wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, {name}!")
+
+    print(f'Congratulations, {name}!')
+
+
+
