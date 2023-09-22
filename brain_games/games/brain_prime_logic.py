25d8c24486c22d0ab20ffd6cb696bbd1c3629f97
jan godar <2017sofin@gmail.com>
2023-09-22 17:50:51 +0300

    brain-prime 1

---
+++ brain_games/games/brain_prime_logic.py
@@ -0,0 +1,20 @@
+import random
+
+def game_quest():
+    '''Brain-prime game question'''
+
+    return ('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+
+def log_prime():
+    '''Game logic brain-prime'''
+
+    random_num = random.randint(1, 100)
+    task = random_num
+
+    correct_answer = 'yes'
+    for i in range(2, random_num//2 + 1):
+        if random_num % i == 0:
+            correct_answer = 'no'
+
+    return correct_answer, task
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,40 +1,10 @@
 #!/usr/bin/env python3
-import random
-import prompt
-
+from brain_games.games.brain_prime_logic import game_quest, log_prime
+from brain_games.games.game import engine_game
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    counter = 0
-
-    while counter != 3:
-        random_number = random.randint(1, 100)
-        flag = True
-        print(f'Question: {random_number}')
-
-        for i in range(2, random_number//2 + 1):
-            if random_number % i == 0:
-                flag = False
-
-        user_answer = prompt.string('Your answer: ')
-        if flag:
-            if user_answer == 'yes':
-                print('Correct!')
-                counter += 1
-            else:
-                print(f'{user_answer} is wrong answer')
-        else:
-            if user_answer == 'yes':
-                print(f'{user_answer} is wrong answer')
-            else:
-                print('Correct')
-                counter += 1
-
-    print(f'Congradulations, {name}!')
-
+    '''Script for calling the game brain-prime'''
+    engine_game(log_prime, game_quest)
 
 if __name__ == "__main__":
     main()
