2d4c64f53b4d715284803ade1b82fda958e13d69
jan godar <2017sofin@gmail.com>
2023-09-22 15:04:55 +0300

    brain-gcd

---
+++ brain_games/games/brain_gcd_logic.py
@@ -0,0 +1,24 @@
+import random
+
+def game_quest():
+    '''Returns the game problem of finding the greatest
+       common divisor'''
+
+    return 'Find the greatest common divisor of given numbers.'
+
+
+def log_gcd():
+    '''Logic for finding the greatest common divisor'''
+
+    first_num = random.randint(1, 100)
+    second_num = random.randint(1, 100)
+    
+    max_num = max(first_num, second_num)
+    min_num = min(first_num, second_num)
+    task = f'{max_num} {min_num}'
+
+    while max_num % min_num != 0:
+        max_num, min_num = min_num, max_num % min_num
+    correct_answer = min_num
+
+    return str(correct_answer), task
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,43 +1,10 @@
 #!/usr/bin/env python3
-import random
-import prompt
-
+from brain_games.games.brain_gcd_logic import game_quest, log_gcd
+from brain_games.games.game import engine_game
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
-
-    print('Find the greatest common divisor of given numbers.')
-    counter = 0
-
-    while counter != 3:
-        first_random_num = random.randint(1, 100)
-        second_random_num = random.randint(1, 100)
-        print(f'Question: {first_random_num} {second_random_num}')
-        user_answer = prompt.integer('Your answer: ')
-
-        if first_random_num == second_random_num:
-            gsd = first_random_num
-        else:
-            max_num = max(first_random_num, second_random_num)
-            min_num = min(first_random_num, second_random_num)
-            flag = True
-            while flag:
-                if max_num % min_num != 0:
-                    max_num, min_num = min_num, max_num % min_num
-                else:
-                    gsd = min_num
-                    flag = False
-
-        if gsd == user_answer:
-            print('Correct!')
-            counter += 1
-        else:
-            print(f'{user_answer} is wrong answer ;(.'
-                  f'Correct answer was {gsd}.\n'
-                  f'Let is try again, {name}')
-    print(f'Congradulations, {name}!')
+    '''Script for calling the game brain-gcd'''
+    engine_game(log_gcd, game_quest)
 
 
 if __name__ == "__main__":
