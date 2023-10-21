1bf2c73c96a9b69c7eb51b745714a89eaa996dd0
jan godar <2017sofin@gmail.com>
2023-10-21 12:13:38 +0300

    step 4.4

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,6 +1,6 @@
 from brain_games.utils import make_num
 from brain_games.constant import QUESTION_BRAIN_PRIME, \
-                                 START_OF_RANGE, END_OF_RANGE
+    START_OF_RANGE, END_OF_RANGE
 from brain_games.core import launches_game
 
 
