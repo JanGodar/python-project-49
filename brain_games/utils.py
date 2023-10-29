ec30a7db23a0657a359fd875c1d99630e8b8c67e
jan godar <2017sofin@gmail.com>
2023-10-29 10:32:32 +0300

    step 7.1

--- brain_games/utils.py
+++ brain_games/utils.py
@@ -2,6 +2,6 @@ import random
 from brain_games.constant import START_RANGE, END_RANGE
 
 
-def get_num():
-    num = random.randint(start = START_RANGE, end = END_RANGE)
+def get_num(start=START_RANGE, end=END_RANGE):
+    num = random.randint(start, end)
     return num
